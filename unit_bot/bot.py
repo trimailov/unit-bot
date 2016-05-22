8bbd5b6bccd17e4e11f977a86b59b886b7fb2770
Justas Trimailovas <j.trimailovas@gmail.com>
2016-05-22 21:27:36 +0300

    Add logging to unit bot

--- unit_bot/bot.py
+++ unit_bot/bot.py
@@ -1,3 +1,5 @@
+import logging
+
 import praw
 
 from unit_bot import creds
@@ -5,6 +7,8 @@ from unit_bot.finder import Finder
 
 
 def scan_and_respond(reply=False, sub='test_unitbot'):
+    logging.basicConfig(filename="info.log", level=logging.INFO)
+
     r = praw.Reddit(user_agent=creds.USER_AGENT)
     r.login(creds.USERNAME, creds.PASSWORD, disable_warning=True)
     del creds.PASSWORD
@@ -19,10 +23,18 @@ def scan_and_respond(reply=False, sub='test_unitbot'):
             if finder.units and comment.id > last_comment and reply:
                 last_comment = comment.id
 
+                info_str = ("Comment body: {}\n"
+                            "Finder: {}").format(comment.body, finder.units)
                 # in case we try to reply to unreplieable comment
                 # e.g. comment got deleted, is to old or etc.
                 # just ignore error then and pass on
                 try:
                     comment.reply(finder.generate_conversion_message())
-                except praw.errors.APIException:
-                    pass
+                    success_str = "Found and replied\n\n"
+                    logging.info(success_str + info_str)
+                except praw.errors.APIException as e:
+                    fail_str = "Exception: {}".format(e)
+                    logging.info(fail_str)
+                except praw.errors.HTTPException as e:
+                    fail_str = "Exception: {}".format(e)
+                    logging.info(fail_str)
